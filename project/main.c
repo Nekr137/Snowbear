174bd100252e10f3d5ee45cb71a3a718d1e96bdc
ndenis <denekra@yandex.ru>
2021-03-11 09:36:49 +0700

    Work around the PWM.

--- project/main.c
+++ project/main.c
@@ -26,10 +26,10 @@
 
 #include "stm32f10x.h"
 
-static uint32_t msTick;
+static volatile uint32_t msTick;
 
 
-// Enableg the PC13 LED
+// Using the PC13 LED as a debugger tool
 static uint32_t isPC13On = 0;
 void Blink(void);
 void BlinkingTool_Config(void);
@@ -44,6 +44,10 @@ void USART1_SendCharacter(volatile uint16_t iData);
 void USART1_SendSTR(volatile uint16_t* iStr);
 void USART1_EraseBuffer(void);
 
+// PWM
+void PWM_Config(void);
+void PWM_SetValue(const uint16_t iValue);
+
 
 // Interruptions
 void SysTick_Handler(void) {
@@ -52,7 +56,7 @@ void SysTick_Handler(void) {
 
 void USART1_IRQHandler(void) {
   if (USART1 -> SR & USART_SR_RXNE) {
-    usartBuf[usartBufIdx++] = USART1->DR;  
+    usartBuf[usartBufIdx++] = USART1->DR;
   }
 }
 
@@ -62,6 +66,7 @@ int main() {
   SysTick_Config(SystemCoreClock/1000);
   BlinkingTool_Config();
   USART1_Config();
+  PWM_Config();
 
   while(1) {
 
@@ -71,8 +76,11 @@ int main() {
       USART1_EraseBuffer();
     }
 
+    // pwm test
+    PWM_SetValue(msTick & 0xFFFF);
+
     // blinking
-    if (msTick>500) {
+    if (msTick>1000) {
       msTick = 0;
       Blink();
     }
@@ -116,13 +124,13 @@ void USART1_Config(void) {
     USART_CR1_RXNEIE; // interruption on
 
   RCC->APB2ENR |= RCC_APB2ENR_IOPAEN | RCC_APB2ENR_AFIOEN; // GPIOA clock ON. Alter function clock ON
-  GPIOA->CRH  &= ~GPIO_CRH_CNF9;   // clear CNF bit 9
-  GPIOA->CRH  |= GPIO_CRH_CNF9_1;  // set CNF bit 9 to 10 - AFIO Push-Pull
-  GPIOA->CRH  |= GPIO_CRH_MODE9_0; // set MODE bit 9 to Mode 01 = 10MHz
+  GPIOA->CRH &= ~GPIO_CRH_CNF9;   // clear CNF bit 9
+  GPIOA->CRH |= GPIO_CRH_CNF9_1;  // set CNF bit 9 to 10 - AFIO Push-Pull
+  GPIOA->CRH |= GPIO_CRH_MODE9_0; // set MODE bit 9 to Mode 01 = 10MHz
 
-  GPIOA->CRH  &= ~GPIO_CRH_CNF10;  // clear CNF bit 9
-  GPIOA->CRH  |= GPIO_CRH_CNF10_0; // set CNF bit 9 to 01 = HiZ
-  GPIOA->CRH  &= ~GPIO_CRH_MODE10; // set MODE bit 9 to Mode 01 = 10MHz
+  GPIOA->CRH &= ~GPIO_CRH_CNF10;  // clear CNF bit 9
+  GPIOA->CRH |= GPIO_CRH_CNF10_0; // set CNF bit 9 to 01 = HiZ
+  GPIOA->CRH &= ~GPIO_CRH_MODE10; // set MODE bit 9 to Mode 01 = 10MHz
 
   NVIC_EnableIRQ(USART1_IRQn);
   __enable_irq();
@@ -140,5 +148,40 @@ void BlinkingTool_Config() {
   GPIOC->CRH |= GPIO_CRH_MODE13_0;
   GPIOC->CRH &= ~GPIO_CRH_CNF13;
   isPC13On = 0;
+}
+
+// ==============================================
+
+void PWM_Config() {
+
+  RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;
+  RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
+  RCC->APB2ENR |= RCC_APB2ENR_AFIOEN;
+
+  GPIOA->CRL &= ~GPIO_CRL_CNF1;
+  GPIOA->CRL |= GPIO_CRL_CNF1_1;
+
+  GPIOA->CRL &= ~GPIO_CRL_MODE1;
+  GPIOA->CRL |= GPIO_CRL_MODE1_1;
+
+  TIM2->CR1  = 0;
+  TIM2->CR2  = 0;
+  TIM2->DIER = 0;
+  
+  TIM2->CCMR1 = TIM_CCMR1_OC2M_1 | TIM_CCMR1_OC2M_2;
+  
+  TIM2->PSC  = 80-1;
+  TIM2->ARR  = 1000;
+  TIM2->CCR2 = 40;
+  TIM2->CCER = TIM_CCER_CC2E;
+  TIM2->EGR  = TIM_EGR_UG;
+  TIM2->CR1 |= TIM_CR1_CEN;
+}
+
+void PWM_SetValue(const uint16_t iValue) {
+  while(!TIM2->SR || !TIM_SR_UIF) {
+  }
+  TIM2->SR |= ~TIM_SR_UIF;
+  TIM2->CCR2 = iValue;
 }
 
